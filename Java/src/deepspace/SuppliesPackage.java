8d0b60541cd537eb7d0b93244478830f6f60095d
SH02-tech <shaojiehu.hu@gmail.com>
2022-03-10 14:19:24 +0100

    Documentation

--- Java/src/deepspace/SuppliesPackage.java
+++ Java/src/deepspace/SuppliesPackage.java
@@ -9,17 +9,28 @@ package deepspace;
 class SuppliesPackage {
     
     // Attributes
-    private float ammoPower;
+    private float ammoPower;    // Armament power. 
     private float fuelUnits;
     private float shieldPower;
     
     // Constructors
+    
+    /** 
+     * @brief Constructor with parameters.
+     * @param theAmmoPower Number corresponding to armamment power of package. 
+     * @param theFuelUnits Number of capacity units of fuel.
+     * @param theShieldPower Number that represents the protection offered by shield. 
+     */
     SuppliesPackage(float theAmmoPower, float theFuelUnits, float theShieldPower) {
         this.ammoPower = theAmmoPower;
         this.fuelUnits = theFuelUnits;
         this.shieldPower = theShieldPower;
     }
     
+    /** 
+     * @brief Copy constructor.
+     * @param s Instance of SuppliesPackage. 
+     */
     SuppliesPackage(SuppliesPackage s) {
         this.ammoPower = s.ammoPower;
         this.fuelUnits = s.fuelUnits;
@@ -28,14 +39,26 @@ class SuppliesPackage {
     
     // Get methods
     
+    /** 
+     * @brief Get method. 
+     * @return Armament power. 
+     */
     float getAmmoPower() {
         return this.ammoPower;
     }
     
+    /** 
+     * @brief Get method. 
+     * @return Fuel units. 
+     */
     float getFuelUnits() {
         return this.fuelUnits;
     }
     
+    /** 
+     * @brief Get method. 
+     * @return Shield power. 
+     */
     float getShieldPower() {
         return this.shieldPower;
     }
