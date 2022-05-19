292c5d81d93446a715e1b1c1ffcdb70f6ca708cb
SH02-tech <shaojiehu.hu@gmail.com>
2022-05-19 09:36:54 +0200

    Damage added

--- Ruby/Damage.rb
+++ Ruby/Damage.rb
@@ -13,149 +13,55 @@ require_relative "WeaponType.rb"
 require_relative "ShieldBooster.rb"
 require_relative "DamageToUI.rb"
 
-    class Damage
+class Damage
 
-        @@NOTUSED = -1
+    # Constructors
 
-        # Constructors
+    private_class_method :new
 
-        def initialize(w, s, wl)
-            @nWeapons = w
-            @nShields = s
+    def initialize(s)
+        @nShields = s
+    end
 
-            if (wl != nil)
-                @weapons  = Array.new(wl)
-            else
-                @weapons = nil
-            end
+    attr_reader :nShields
+
+    # TODO
+    def copy()
+    end
+
+    def getUIversion()
+    end
+
+    def adjustShields(s)
+        return [nShields,s.length].min
+    end
+    private :adjustShields
+
+    # TODO
+    def adjust(w, s)    # s : shieldBooster[] 
+        adjustShields(s)
+    end
+    
+    def discardShieldBooster()
+        if @nShields > 0
+            @nShields -= 1
+        else
+            @nShields = 0
         end
+        return nil
+    end
 
-        attr_reader :nShields, :nWeapons, :weapons
+    def discardWeapon(weapon)
+    end
 
-        def self.newNumericWeapons(w, s)
-            new(w, s, nil)
-        end
+    def hasNoEffect
+        nShields == 0
+    end
 
-        def self.newSpecificWeapons(wl, s)  # wl : WeaponType[]
-            new(@@NOTUSED, s, wl)
-        end
+    def to_s
+        return "[nShields: #{nShields}]"
+    end
 
-        private_class_method :new
-
-        def self.newCopy(d)
-            if d.nWeapons == @@NOTUSED   # Using an array
-                newSpecificWeapons(d.weapons, d.nShields)
-            else # Not using an array
-                newNumericWeapons(d.nWeapons, d.nShields)
-            end
-        end
-
-        def getUIversion()
-            DamageToUI.new(self)
-        end
-
-        def arrayContainsType(w, t)     # w : Weapon[], t :WeaponType
-            index = -1
-            count = 0
-            found = false
-            while count < w.length && !found
-                if (w[count].type == t)
-                    index = count
-                    found = true
-                else
-                    count += 1
-                end
-            end
-            return index
-        end
-
-        private :arrayContainsType
-
-        def adjust(w, s)    # w : Weapon[], s : shieldBooster[] 
-
-            if nWeapons == @@NOTUSED   # Using an array
-                
-                weaponsCopy = Array.new(weapons)
-                wCopy = Array.new(w)
-                index = 0
-
-                while index < weaponsCopy.length
-                    weaponType = weaponsCopy[index]
-                    pos = arrayContainsType(wCopy,weaponType)
-                    if pos >= 0
-                        wCopy.delete_at(pos)
-                        index += 1
-                    else
-                        weaponsCopy.delete_at(index)
-                    end
-                end
-
-                nShieldsCopy = [nShields,s.length].min
-                
-                return Damage.newSpecificWeapons(weaponsCopy, nShieldsCopy)
-
-            else  # Not using an array
-                nWeaponsCopy = [nWeapons,w.length].min
-
-                nShieldsCopy = [nShields,s.length].min
-
-                return Damage.newNumericWeapons(nWeaponsCopy, nShieldsCopy)
-            end
-        end
-
-        def discardWeapon(w)    # w : weaponn
-            if @nWeapons == @@NOTUSED && @weapons.length > 0 # Using an array
-                @weapons.delete(w.type)
-            else  # Not using an array
-                if @nWeapons > 0 
-                    @nWeapons -= 1
-                else
-                    @nWeapons = 0
-                end
-            end
-            return nil
-        end
-        
-        def discardShieldBooster()
-            if @nShields > 0
-                @nShields -= 1
-            else
-                @nShields = 0
-            end
-            return nil
-        end
-
-        def hasNoEffect
-            if nWeapons == @@NOTUSED # Using an array
-                nShields == 0 && weapons.length == 0
-            else # Not using an array
-                nShields == 0 && nWeapons == 0
-            end
-        end
-
-        def to_s
-
-            if nWeapons == @@NOTUSED 
-
-                weaponsString = ""
-                weapons.each do |w|
-                    weaponsString += "#{w.to_s}, "
-                end
-
-                s = "["
-                s += "nShields: #{nShields}"
-                s += "; weaponsType: #{weaponsString}"
-                s += "]"
-                return s
-            else
-                s = "["
-                s += "nShields: #{nShields}"
-                s += "; nWeapons: #{nWeapons}"
-                s += "]"
-                return s
-            end
-        end
-
-    end # Damage
+end # Damage
 
 end # Deepspace
---
+++ Ruby/SpecificDamage.rb
@@ -0,0 +1,156 @@
+#encoding:utf-8
+
+# +-------------------------------------------------------------------+ #
+# | DEEPSPACE (Ruby)                                                  | #
+# | Authors: Shao Jie Hu Chen and Mario Megías Mateo                  | #
+# +-------------------------------------------------------------------+ #
+# | Specific Damage.                                                  | #
+# +-------------------------------------------------------------------+ #
+
+
+module Deepspace
+
+require_relative 'Damage.rb'
+require_relative 'Weapon.rb'
+require_relative 'ShieldBooster.rb'
+require_relative 'SpecificDamageToUI.rb'
+
+class SpecificDamage < Damage
+    def initialize(wl,s)
+        super(s)
+        @weapons = Array.new(wl)
+    end
+
+    public_class_method :new
+
+    attr_reader :weapons
+
+    # Override
+    def copy
+        return SpecificDamage.new(weapons, nShields)
+    end
+
+    # Override
+    def getUIversion
+        return SpecificDamageToUI.new(self)
+    end
+
+    def arrayContainsType(weapon_arr, type)
+        index = -1
+        count = 0
+        found = false
+        while count < weapon_arr.length && !found
+            if (weapon_arr[count].type == type)
+                index = count
+                found = true
+            else
+                count += 1
+            end
+        end
+        return index
+    end
+
+    private :arrayContainsType
+
+    # Override
+    def adjust(theWeaponsArray, theShieldsArray)
+        nShieldsCopy = super
+
+        weaponsCopy = Array.new(weapons)
+        wCopy = Array.new(theWeaponsArray)
+        index = 0
+
+        while index < weaponsCopy.length
+            weaponType = weaponsCopy[index]
+            pos = arrayContainsType(wCopy,weaponType)
+            if pos >= 0
+                wCopy.delete_at(pos)
+                index += 1
+            else
+                weaponsCopy.delete_at(index)
+            end
+        end
+
+        return SpecificDamage.new(wCopy, nShieldsCopy)
+    end
+
+    # Override
+    def discardWeapon(weapon)
+        index = arrayContainsType(@weapons, weapon.type)
+
+        if index >= 0
+            @weapons.delete_at(index)
+        end
+    end
+
+    # Override
+    def hasNoEffect
+        return (super && (weapons.length == 0))
+    end
+
+    # Override
+    def to_s
+        s = super
+        s2 = s.sub("]", "")
+
+        weaponsString = ""
+        weapons.each do |w|
+            weaponsString += "#{w.to_s}, "
+        end
+        
+        s2 += "; weaponsType: #{weaponsString}]"
+
+        return s2
+    end
+
+end # SpecificDamage
+end # Deepspace
+
+if $0 == __FILE__ then
+
+    w1 = Deepspace::Weapon.new("Laser 1", Deepspace::WeaponType::LASER, 10)
+    w2 = Deepspace::Weapon.new("Laser 2", Deepspace::WeaponType::LASER, 20)
+    w3 = Deepspace::Weapon.new("Missile 1", Deepspace::WeaponType::MISSILE, 10)
+    w4 = Deepspace::Weapon.new("Missile 2", Deepspace::WeaponType::MISSILE, 10)
+    
+    s1 = Deepspace::ShieldBooster.new("ShieldBooster 1", 10, 2)
+    s2 = Deepspace::ShieldBooster.new("ShieldBooster 2", 20, 7)
+
+    puts '-----------------------------------------------'
+    puts 'Constructor'
+    puts '-----------------------------------------------'
+    specDamage = Deepspace::SpecificDamage.new([w1,w2,w3,w4], 10)
+    puts specDamage.to_s
+
+    puts "\n\n"
+
+    puts '-----------------------------------------------'
+    puts 'Copy'
+    puts '-----------------------------------------------'
+    copyDamage = specDamage.copy
+    puts copyDamage.to_s
+
+    puts "\n\n"
+
+    puts '-----------------------------------------------'
+    puts 'UI version'
+    puts '-----------------------------------------------'
+    uiDamage = specDamage.getUIversion
+    puts uiDamage.to_s
+
+    puts "\n\n"
+
+    puts '-----------------------------------------------'
+    puts 'Adjust'
+    puts '-----------------------------------------------'
+    
+    adjustDamage = specDamage.adjust([w1], [s1,s2])
+
+    puts adjustDamage.to_s
+    puts specDamage.to_s
+
+    puts "\n\n"
+
+    
+    
+end # Test
