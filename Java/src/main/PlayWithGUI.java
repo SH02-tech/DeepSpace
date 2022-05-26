945e94598a8548071c9dfe0323e67ca29eccd4ba
SH02-tech <shaojiehu.hu@gmail.com>
2022-05-26 13:49:52 +0200

    Selection of names added

---
+++ Java/src/main/PlayWithGUI.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import View.UI.GraphicMainView;
+import View.DeepSpaceView;
+import controller.Controller;
+import deepspace.GameUniverse;
+
+/**
+ *
+ * @author shao
+ */
+public class PlayWithGUI {
+    public static void main(String[] args) {
+        DeepSpaceView ui;
+        GameUniverse game = new GameUniverse();
+        ui = GraphicMainView.getInstance();
+        Controller controller = Controller.getInstance();
+        controller.setModelView(game,ui);
+        controller.start(); 
+    }
+}
